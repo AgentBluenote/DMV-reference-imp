b245e0ee10de3f27f3052848db5e36bdf44d9963
Michael <monolisa56@gmail.com>
2021-01-29 08:53:43 -0500

    initial turnover for DMV-software

---
+++ DMV-software/src/org/dmv/client/DMVClient.java
@@ -0,0 +1,30 @@
+package org.dmv.client;
+
+public class DMVClient {
+
+
+   /*
+    *   this is a test comment.iii 
+    *   this is a test comment 2
+    *   this is a test comment 2
+    */
+
+   /*
+    *    main() entry point.   
+    */
+	public static void main(String[] args) {
+
+        DMVClient client = new DMVClient();
+
+    }
+	
+   /*
+    *  Constructor
+    */
+	DMVClient(){
+        Person p = new Person();
+
+        System.out.println("I am the DMV client");  		
+	}  
+
+} // end of class 
---
+++ DMV-software/src/org/dmv/client/Person.java
@@ -0,0 +1,26 @@
+package org.dmv.client;
+
+// test comment 
+
+import java.time.LocalDate;
+
+public class Person {
+
+    public enum Sex {
+        MALE, FEMALE
+    }
+
+    String name;
+    LocalDate birthday;
+    Sex gender;
+    String emailAddress;
+
+    public int getAge() {
+		return 0;
+        // ...
+    }
+
+    public void printPerson() {
+        // ...
+    }
+}
