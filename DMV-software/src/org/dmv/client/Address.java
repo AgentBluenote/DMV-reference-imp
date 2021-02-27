f9081dbffab0ac146d671760ecc7d6736b828426
Michael <monolisa56@gmail.com>
2021-02-27 08:06:22 -0500

    updated Address class

---
+++ DMV-software/src/org/dmv/client/Address.java
@@ -0,0 +1,42 @@
+package org.dmv.client;
+
+
+// 
+//  Driver's license 
+//  REAL ID 
+//  Change my address
+//  Insurance carierr  
+//  Get my driving record
+// c
+
+public class Address {
+    private String m_street_address = new String();
+    private String m_city = new String();
+    private String m_state = new String();
+    private String m_zipCode = new String();
+
+    
+	public Address() {
+		// TODO Auto-generated constructor stub
+	}
+
+   /*	
+    * 
+    * 
+    */
+	void setStreetAddress( String newAddress ){
+        m_street_address = newAddress;	    
+		
+
+	}
+	
+	
+    //
+    // main()
+    //
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
